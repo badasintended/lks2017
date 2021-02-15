df039e9fc7898761d6b4178da51f1b7fefa35086
deirn <foreirn@gmail.com>
2021-02-15 14:29:21 +0700

    final

--- Forms/FormEditProfile.Designer.cs
+++ Forms/FormEditProfile.Designer.cs
@@ -161,6 +161,7 @@
             this.button4.Size = new System.Drawing.Size(50, 50);
             this.button4.TabIndex = 12;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label8
             // 
--- Forms/FormEditProfile.cs
+++ Forms/FormEditProfile.cs
@@ -7,18 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Nusantara.Forms
 {
     public partial class FormEditProfile : Form
     {
+        private static readonly string ProfilePictures = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\lks2017\profilepictures\";
+
         private readonly Form nav;
+
+        private string oldPfp = null;
+
         public FormEditProfile(Form nav)
         {
             InitializeComponent();
 
+            if (!Directory.Exists(ProfilePictures))
+            {
+                Directory.CreateDirectory(ProfilePictures);
+            }
+
             this.nav = nav;
 
+            Clear();
+        }
+
+        private void Clear()
+        {
             ErrorLabel.Text = "";
 
             var student = Program.User.Student;
@@ -30,6 +46,11 @@ namespace Nusantara.Forms
                 NameBox.Text = student.Name;
                 PhoneBox.Text = student.PhoneNumber;
                 AddressBox.Text = student.Address;
+
+                if (student.Photo != null)
+                {
+                    oldPfp = ProfilePictures + student.StudentId + "-" + student.Photo;
+                }
             }
 
             if (teacher != null)
@@ -38,6 +59,19 @@ namespace Nusantara.Forms
                 NameBox.Text = teacher.Name;
                 PhoneBox.Text = teacher.PhoneNumber;
                 AddressBox.Text = teacher.Address;
+
+                if (teacher.Photo != null)
+                {
+                    oldPfp = ProfilePictures + teacher.TeacherId + "-" + teacher.Photo;
+                }
+            }
+
+            try
+            {
+                pictureBox1.Image = Image.FromFile(oldPfp);
+            }
+            catch {
+                oldPfp = null;
             }
         }
 
@@ -75,7 +109,8 @@ namespace Nusantara.Forms
             var student = Program.User.Student;
             var teacher = Program.User.Teacher;
 
-            if (student != null) {
+            if (student != null)
+            {
                 student.Name = name;
                 student.PhoneNumber = phone;
                 student.Address = address;
@@ -100,6 +135,38 @@ namespace Nusantara.Forms
         private void FormEditProfile_FormClosed(object sender, FormClosedEventArgs e)
         {
             nav.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var path = dialog.FileName;
+                var file = Path.GetFileName(path);
+
+                if (oldPfp != null) File.Delete(oldPfp);
+
+                File.Copy(path, ProfilePictures + Program.User.username + "-" + file);
+
+                var student = Program.User.Student;
+                var teacher = Program.User.Teacher;
+
+                if (student != null)
+                {
+                    student.Photo = file;
+                }
+
+                if (teacher != null)
+                {
+                    teacher.Photo = file;
+                }
+
+                Program.Entities.SaveChanges();
+                Clear();
+            }
         }
     }
 }
